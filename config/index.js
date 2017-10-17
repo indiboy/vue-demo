9712770659a88aeadabbe1175e91009e969546cd
tianlu <1074025714@qq.com>
2017-10-17 09:17:31 +0800

    修改dist

--- config/index.js
+++ config/index.js
@@ -11,7 +11,10 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    //原来的
+    //assetsPublicPath: '/',
+    //修改绝对路径为相对路径
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
