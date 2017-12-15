d3b1c251580b645127489d49d74ecb010c8e4830
tianlu <1074025714@qq.com>
2017-12-15 09:47:29 +0800

    vue demo

---
+++ src/page/ele.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+    <el-table
+            :data="tableData"
+            style="width: 100%">
+        <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="address"
+                label="地址">
+        </el-table-column>
+    </el-table>
+    <el-alert
+            title="成功提示的文案"
+            type="success"
+            center
+            show-icon>
+    </el-alert>
+    <el-alert
+            title="消息提示的文案"
+            type="info"
+            center
+            show-icon>
+    </el-alert>
+    <el-alert
+            title="警告提示的文案"
+            type="warning"
+            center
+            show-icon>
+    </el-alert>
+    <el-alert
+            title="错误提示的文案"
+            type="error"
+            center
+            show-icon>
+    </el-alert>
+        <el-pagination
+                background
+                layout="prev, pager, next"
+                :total="1000">
+        </el-pagination>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tableData: [{
+                    date: '2016-05-02',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1518 弄'
+                }, {
+                    date: '2016-05-04',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1517 弄'
+                }, {
+                    date: '2016-05-01',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1519 弄'
+                }, {
+                    date: '2016-05-03',
+                    name: '王小虎',
+                    address: '上海市普陀区金沙江路 1516 弄'
+                }]
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang='scss'>
+
+</style>
